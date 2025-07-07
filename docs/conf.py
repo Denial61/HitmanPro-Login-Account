ad2475ccaeb9730e498d8910e4fb9903040d7139
Denial61 <hs51525254@gmail.com>
2025-07-07 13:04:29 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,15 @@
-project = 'HitmanPro Login Account'
+project = 'At&t'
 author = 'Denial'
 release = '1.0'
-
+ 
 extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
-
+ 
 html_theme = 'alabaster'
-html_static_path = ['_static']
+html_static_path = ['_static']   
+ 
+html_js_files = [
+    'chatbot.js',
+]
+html_favicon = '_static/hitman-pro.png'
